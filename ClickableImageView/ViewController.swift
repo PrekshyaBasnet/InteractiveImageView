4a62a0e50129cda1a72dd7712faeb8e28ce8c5f9
Prekshya Admin <prekshya.smartmobe@gmail.com>
2018-07-03 15:59:19 +0545

    initial commit

--- ClickableImageView/ViewController.swift
+++ ClickableImageView/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct SampleItem {
+    var imageName = ""
+    var imagePoints: [[CGFloat]] = []
+    init(imageName: String, imagePoint: [[CGFloat]]) {
+        self.imageName = imageName
+        self.imagePoints = imagePoint
+    }
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var customClickableView: UIView!
+    var scrollView: CustomScrollView!
+    var sampleItem: SampleItem?
+    var image: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureSampleModel()
+        setupScrollImageView()
+        scrollView.centerImage()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func configureSampleModel() {
+        sampleItem = SampleItem(imageName: SampleModel.names.first!, imagePoint: SampleModel.bodyPartsFront)
+        if let sampleItem = sampleItem {
+            guard let image = UIImage(named: sampleItem.imageName) else { return }
+            self.image = image
+        }
     }
+    
+    func setupScrollImageView() {
+        let frame = CGRect(x: 0, y: 0, width: self.customClickableView.frame.width, height: self.customClickableView.frame.height)
+        guard let minimumZoom = minimumZoom() else { return }
+        scrollView = CustomScrollView(frame, minZoom: minimumZoom, maxZoom: 3.0)
+        scrollView.customdelegate = self
+        if let sampleItem = sampleItem, let image = image {
+            scrollView.getPictureViewWithPoints(points: sampleItem.imagePoints, image: image)
+        }
+         self.customClickableView.addSubview(scrollView)
+    }
+    
+    func minimumZoom() -> CGFloat? {
+        guard let image = image else { return nil }
+        return min((self.customClickableView.bounds.size.width)/(image.size.width),(self.customClickableView.bounds.size.height)/(image.size.height))
+    }
+    
+    func paint(view: UIView, color: UIColor = .red) {
+        view.backgroundColor = color
+    }
+}
 
-
+extension ViewController: CustomScrollViewDelegate {
+    func viewSelected(_ selected: Bool, index: Int) {
+            let selectedView = scrollView.pictureView.subviews[index+1]
+            selected ? paint(view: selectedView) : paint(view: selectedView, color: .clear)
+    }
 }
 
